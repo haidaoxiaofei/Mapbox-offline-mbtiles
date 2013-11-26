ec477c5201314ebda09753078603656d13f594bc
Francisco Dans <fdansv@gmail.com>
2013-11-26 16:37:43 +0000

    added marker factory

---
+++ src/src/com/mapbox/mapboxsdk/MarkerFactory.java
@@ -0,0 +1,11 @@
+package com.mapbox.mapboxsdk;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: fdans
+ * Date: 26/11/13
+ * Time: 16:37
+ * To change this template use File | Settings | File Templates.
+ */
+public class MarkerFactory {
+}
