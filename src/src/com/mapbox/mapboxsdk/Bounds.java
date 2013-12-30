be148bf3fc79fdac9a3273955a56a92b27e6af12
Francisco Dans <fdansv@gmail.com>
2013-12-30 22:48:31 +0100

    added getters to bounds

--- src/src/com/mapbox/mapboxsdk/Bounds.java
+++ src/src/com/mapbox/mapboxsdk/Bounds.java
@@ -4,5 +4,33 @@ package com.mapbox.mapboxsdk;
  * Created by Francisco on 30/12/13.
  */
 public class Bounds {
-    double left, bottom, right, top;
+    private double left;
+    private double bottom;
+    private double right;
+    private double top;
+
+    public Bounds(double left, double bottom, double right, double top){
+        this.left = left;
+        this.bottom = bottom;
+        this.right = right;
+        this.top = top;
+    }
+
+    public double getTop() {
+        return top;
+    }
+
+    public double getLeft() {
+        return left;
+    }
+
+    public double getBottom() {
+        return bottom;
+    }
+
+    public double getRight() {
+        return right;
+    }
+
+
 }
