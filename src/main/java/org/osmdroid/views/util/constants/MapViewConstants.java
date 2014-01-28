147565d1b35ab3e67044da9de7a5e193d67ff226
Tom MacWright <tom@macwright.org>
2014-01-28 11:22:54 -0500

    Make zoom animations twice as fast. 1s is too long

--- src/main/java/org/osmdroid/views/util/constants/MapViewConstants.java
+++ src/main/java/org/osmdroid/views/util/constants/MapViewConstants.java
@@ -19,8 +19,8 @@ public interface MapViewConstants {
     public static final int ANIMATION_SMOOTHNESS_DEFAULT = 10;
     public static final int ANIMATION_SMOOTHNESS_HIGH = 20;
 
-    public static final int ANIMATION_DURATION_SHORT = 500;
-    public static final int ANIMATION_DURATION_DEFAULT = 1000;
+    public static final int ANIMATION_DURATION_SHORT = 250;
+    public static final int ANIMATION_DURATION_DEFAULT = 500;
     public static final int ANIMATION_DURATION_LONG = 2000;
 
     /**
