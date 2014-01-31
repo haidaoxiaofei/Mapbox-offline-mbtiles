b113a9983bf84ccd81bfdd1351aa8ab62e27ad0e
Francisco Dans <fdansv@gmail.com>
2014-01-31 01:34:54 +0000

    fixes build.sh to use gradle 1.9

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ echo Mapbox Android SDK builder
 echo
 echo
 
-curl -O http://downloads.gradle.org/distributions/gradle-1.8-bin.zip
-unzip gradle-1.8-bin
-gradle-1.8/bin/gradle build
+curl -O http://downloads.gradle.org/distributions/gradle-1.9-bin.zip
+unzip gradle-1.9-bin
+gradle-1.9/bin/gradle build
 mv build/apk/mapbox-android-sdk-debug-unaligned.apk
