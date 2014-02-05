60a56fb9453d0773ede73806764eb6c406e32daa
Francisco Dans <fdansv@gmail.com>
2014-02-05 12:10:56 -0500

    adds tooltip to views package

---
+++ src/src/com/mapbox/mapboxsdk/views/Tooltip.java
@@ -0,0 +1,14 @@
+package com.mapbox.mapboxsdk.views;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+
+
+public class Tooltip extends View {
+
+    public Tooltip(Context context) {
+        super(context);
+    }
+
+}
