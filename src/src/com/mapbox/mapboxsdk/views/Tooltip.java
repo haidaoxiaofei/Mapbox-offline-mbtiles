2ccbfd6646a9cf77967a478c9558355259546d56
Francisco Dans <fdansv@gmail.com>
2014-02-05 13:46:32 -0500

    adds basic tooltip class structure

--- src/src/com/mapbox/mapboxsdk/views/Tooltip.java
+++ src/src/com/mapbox/mapboxsdk/views/Tooltip.java
@@ -1,14 +1,29 @@
 package com.mapbox.mapboxsdk.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
 import android.view.View;
 
 
 public class Tooltip extends View {
 
-    public Tooltip(Context context) {
+    private final MapView parentMapView;
+    private String description;
+    private String title;
+
+    public Tooltip(Context context, MapView mapView) {
         super(context);
+        parentMapView = mapView;
     }
 
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+    }
+
+    public void setText(String title, String description){
+        this.title = title;
+        this.description = description;
+    }
 }
