dea365178977217abe8f0e37d69e6a507385edcb
Francisco Dans <fdansv@gmail.com>
2014-02-07 17:24:05 -0500

    adds proper tooltip default view xml

---
+++ src/src/com/mapbox/mapboxsdk/overlay/ExtendedOverlayItem.java
@@ -0,0 +1,7 @@
+package com.mapbox.mapboxsdk.overlay;
+
+/**
+ * Created by Francisco on 07/02/14.
+ */
+public class ExtendedOverlayItem {
+}
---
+++ src/src/com/mapbox/mapboxsdk/views/DefaultInfoWindow.java
@@ -0,0 +1,7 @@
+package com.mapbox.mapboxsdk.views;
+
+/**
+ * Created by Francisco on 07/02/14.
+ */
+public class DefaultInfoWindow {
+}
