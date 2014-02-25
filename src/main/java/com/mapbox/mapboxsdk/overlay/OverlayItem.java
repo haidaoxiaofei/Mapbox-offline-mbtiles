f6162a4b4eec4c5f7db33c6af6519bd2d3814356
Francisco Dans <fdansv@gmail.com>
2014-02-25 15:18:08 +0000

    adds grouping members to overlayitem

--- src/main/java/com/mapbox/mapboxsdk/overlay/OverlayItem.java
+++ src/main/java/com/mapbox/mapboxsdk/overlay/OverlayItem.java
@@ -23,6 +23,15 @@ public class OverlayItem {
     public static final int ITEM_STATE_SELECTED_MASK = 2;
 
     protected static final Point DEFAULT_MARKER_SIZE = new Point(26, 94);
+    private int group = 0;
+
+    public int getGroup() {
+        return group;
+    }
+
+    public void assignGroup(int currentGroup) {
+        group = currentGroup;
+    }
 
     /**
      * Indicates a hotspot for an area. This is where the origin (0,0) of a point will be located
