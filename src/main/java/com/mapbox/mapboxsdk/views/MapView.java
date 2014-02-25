195ef8bca02fa30b2494d31d5ce150675c03b53e
Francisco Dans <fdansv@gmail.com>
2014-02-25 15:11:44 +0000

    adds screenX and screenY temporarily

--- src/main/java/com/mapbox/mapboxsdk/views/MapView.java
+++ src/main/java/com/mapbox/mapboxsdk/views/MapView.java
@@ -293,7 +293,15 @@ public class MapView extends ViewGroup implements MapViewConstants, MapEventsRec
             }
         }
     }
-    
+
+    private double screenX(OverlayItem item){
+        return mProjection.toPixels(item.getPoint(), null).x;
+    }
+
+    private double screenY(OverlayItem item){
+        return mProjection.toPixels(item.getPoint(), null).y;
+    }
+
 
     private double dist(double x1, double y1, double x2, double y2){
         return Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
