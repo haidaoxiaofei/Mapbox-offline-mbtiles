639c354935a3e641c1d17235cc3a9c4163225340
Francisco Dans <fdansv@gmail.com>
2014-03-11 08:41:03 +0000

    Adds ClusterItem as subclass of Marker

---
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/ClusterItem.java
@@ -0,0 +1,27 @@
+package com.mapbox.mapboxsdk.overlay;
+
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.views.MapView;
+
+/**
+ * Created by Francisco Dans on 06/03/14.
+ */
+public class ClusterItem extends Marker{
+    private int childCount = 0;
+
+    public ClusterItem(MapView mv, String aTitle, String aDescription, LatLng aLatLng) {
+        super(mv, aTitle, aDescription, aLatLng);
+    }
+
+    public ClusterItem(String aTitle, String aSnippet, LatLng aLatLng) {
+        super(aTitle, aSnippet, aLatLng);
+    }
+
+    public int getChildCount() {
+        return childCount;
+    }
+
+    public void setChildCount(int childCount) {
+        this.childCount = childCount;
+    }
+}
