29895533e2fd2050d2de7b199b607e4400f56f03
Martin Guillon <martin.guillon@akylas.fr>
2014-03-14 10:10:18 +0100

    some changes on the example

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MainActivity.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MainActivity.java
@@ -18,6 +18,7 @@ import com.mapbox.mapboxsdk.overlay.Icon;
 import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.overlay.PathOverlay;
 import com.mapbox.mapboxsdk.overlay.UserLocationOverlay;
+import com.mapbox.mapboxsdk.tileprovider.tilesource.ITileLayer;
 import com.mapbox.mapboxsdk.tileprovider.tilesource.MapboxTileLayer;
 import com.mapbox.mapboxsdk.views.MapController;
 import com.mapbox.mapboxsdk.views.MapView;
@@ -25,170 +26,191 @@ import com.mapbox.mapboxsdk.views.util.TilesLoadedListener;
 
 public class MainActivity extends ActionBarActivity {
 
-    private MapController mapController;
-    private LatLng startingPoint = new LatLng(51f, 0f);
-    private MapView mv;
-    private UserLocationOverlay myLocationOverlay;
-    private Paint paint;
-    private String satellite = "brunosan.map-cyglrrfu";
-    private String street = "examples.map-vyofok3q";
-    private String terrain = "examples.map-zgrqqx0w";
-    private String currentLayer = "terrain";
-    private PathOverlay equator;
+	private MapController mapController;
+	private LatLng startingPoint = new LatLng(51f, 0f);
+	private MapView mv;
+	private UserLocationOverlay myLocationOverlay;
+	private Paint paint;
+	private String satellite = "brunosan.map-cyglrrfu";
+	private String street = "examples.map-vyofok3q";
+	private String terrain = "examples.map-zgrqqx0w";
+	private String currentLayer = "terrain";
+	private PathOverlay equator;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
-        setContentView(R.layout.activity_main);
-        mv = (MapView)findViewById(R.id.mapview);
-        mapController = mv.getController();
-        mv.setCenter(startingPoint).setZoom(4);
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+		setContentView(R.layout.activity_main);
+		mv = (MapView) findViewById(R.id.mapview);
+		replaceMapView(terrain);
+		mv.setCenter(startingPoint).setZoom(4);
 
-        mv.loadFromGeoJSONURL("https://gist.github.com/fdansv/8541618/raw/09da8aef983c8ffeb814d0a1baa8ecf563555b5d/geojsonpointtest");
-        setButtonListeners();
-        Marker m = new Marker(mv, "Edinburgh", "Scotland", new LatLng(55.94629, -3.20777));
-        m.setIcon(new Icon(getResources(), Icon.Size.SMALL, "marker-stroked", "FF0000"));
-        mv.addMarker(m);
+		mv.loadFromGeoJSONURL("https://gist.github.com/fdansv/8541618/raw/09da8aef983c8ffeb814d0a1baa8ecf563555b5d/geojsonpointtest");
+		setButtonListeners();
+		Marker m = new Marker(mv, "Edinburgh", "Scotland", new LatLng(55.94629,
+				-3.20777));
+		m.setIcon(new Icon(getResources(), Icon.Size.SMALL, "marker-stroked",
+				"FF0000"));
+		mv.addMarker(m);
 
-        m = new Marker(mv, "Stockholm", "Sweden", new LatLng(59.32995, 18.06461));
-        m.setIcon(new Icon(getResources(), Icon.Size.MEDIUM, "city", "FFFF00"));
-        mv.addMarker(m);
+		m = new Marker(mv, "Stockholm", "Sweden",
+				new LatLng(59.32995, 18.06461));
+		m.setIcon(new Icon(getResources(), Icon.Size.MEDIUM, "city", "FFFF00"));
+		mv.addMarker(m);
 
-        m = new Marker(mv, "Prague", "Czech Republic", new LatLng(50.08734, 14.42112));
-        m.setIcon(new Icon(getResources(), Icon.Size.LARGE, "land-use", "00FFFF"));
-        mv.addMarker(m);
+		m = new Marker(mv, "Prague", "Czech Republic", new LatLng(50.08734,
+				14.42112));
+		m.setIcon(new Icon(getResources(), Icon.Size.LARGE, "land-use",
+				"00FFFF"));
+		mv.addMarker(m);
 
-        m = new Marker(mv, "Athens", "Greece", new LatLng(37.97885, 23.71399));
-        mv.addMarker(m);
+		m = new Marker(mv, "Athens", "Greece", new LatLng(37.97885, 23.71399));
+		mv.addMarker(m);
 
-        mv.setOnTilesLoadedListener(new TilesLoadedListener() {
-            @Override
-            public boolean onTilesLoaded() {
-                return false;
-            }
-        });
-        mv.setVisibility(View.VISIBLE);
-        equator = new PathOverlay();
-        equator.addPoint(0,-89);
-        equator.addPoint(0, 89);
-        mv.getOverlays().add(equator);
-    }
+		mv.setOnTilesLoadedListener(new TilesLoadedListener() {
+			@Override
+			public boolean onTilesLoaded() {
+				return false;
+			}
 
-    private void setButtonListeners() {
-        Button satBut = changeButtonTypeface((Button)findViewById(R.id.satbut));
-        satBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!currentLayer.equals("satellite")) {
-                    replaceMapView(satellite);
-                    currentLayer = "satellite";
-                }
-            }
-        });
-        Button terBut = changeButtonTypeface((Button)findViewById(R.id.terbut));
-        terBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!currentLayer.equals("terrain")) {
-                    replaceMapView(terrain);
-                    currentLayer = "terrain";
-                }
-            }
-        });
-        Button strBut = changeButtonTypeface((Button)findViewById(R.id.strbut));
-        strBut.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (!currentLayer.equals("street")) {
-                    replaceMapView(street);
-                    currentLayer = "street";
-                }
-            }
-        });
-    }
+			@Override
+			public boolean onTilesLoadStarted() {
+				// TODO Auto-generated method stub
+				return false;
+			}
+		});
+		mv.setVisibility(View.VISIBLE);
+		equator = new PathOverlay();
+		equator.addPoint(0, -89);
+		equator.addPoint(0, 89);
+		mv.getOverlays().add(equator);
+	}
 
-    protected void replaceMapView(String layer) {
-        mv.setTileSource(new MapboxTileLayer(layer));
-    }
+	private void setButtonListeners() {
+		Button satBut = changeButtonTypeface((Button) findViewById(R.id.satbut));
+		satBut.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!currentLayer.equals("satellite")) {
+					replaceMapView(satellite);
+					currentLayer = "satellite";
+				}
+			}
+		});
+		Button terBut = changeButtonTypeface((Button) findViewById(R.id.terbut));
+		terBut.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!currentLayer.equals("terrain")) {
+					replaceMapView(terrain);
+					currentLayer = "terrain";
+				}
+			}
+		});
+		Button strBut = changeButtonTypeface((Button) findViewById(R.id.strbut));
+		strBut.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				if (!currentLayer.equals("street")) {
+					replaceMapView(street);
+					currentLayer = "street";
+				}
+			}
+		});
+	}
 
-    private void addLocationOverlay() {
-        // Adds an icon that shows location
-        myLocationOverlay = new UserLocationOverlay(this, mv);
-        myLocationOverlay.enableMyLocation();
-        myLocationOverlay.setDrawAccuracyEnabled(true);
-        mv.getOverlays().add(myLocationOverlay);
-    }
-    private void addLine() {
-        // Configures a line
-        PathOverlay po = new PathOverlay(Color.RED, this);
-        Paint linePaint = new Paint();
-        linePaint.setStyle(Paint.Style.STROKE);
-        linePaint.setColor(Color.BLUE);
-        linePaint.setStrokeWidth(5);
-        po.setPaint(linePaint);
-        po.addPoint(startingPoint);
-        po.addPoint(new LatLng(51.7, 0.3));
-        po.addPoint(new LatLng(51.2, 0));
+	protected void replaceMapView(String layer) {
+		ITileLayer source = new MapboxTileLayer(layer);
+		mv.setScrollableAreaLimit(source.getBoundingBox());
+		mv.setMinZoomLevel(source.getMinimumZoomLevel());
+		mv.setMaxZoomLevel(source.getMaximumZoomLevel());
+		mv.setTileSource(source);
+		mv.zoomToBoundingBox(source.getBoundingBox());
+	}
 
-        // Adds line and marker to the overlay
-        mv.getOverlays().add(po);
-    }
+	private void addLocationOverlay() {
+		// Adds an icon that shows location
+		myLocationOverlay = new UserLocationOverlay(this, mv);
+		myLocationOverlay.enableMyLocation();
+		myLocationOverlay.setDrawAccuracyEnabled(true);
+		mv.getOverlays().add(myLocationOverlay);
+	}
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
+	private void addLine() {
+		// Configures a line
+		PathOverlay po = new PathOverlay(Color.RED, this);
+		Paint linePaint = new Paint();
+		linePaint.setStyle(Paint.Style.STROKE);
+		linePaint.setColor(Color.BLUE);
+		linePaint.setStrokeWidth(5);
+		po.setPaint(linePaint);
+		po.addPoint(startingPoint);
+		po.addPoint(new LatLng(51.7, 0.3));
+		po.addPoint(new LatLng(51.2, 0));
 
-        return true;
-    }
-    private Button changeButtonTypeface(Button button){
-        //Typeface tf = Typeface.createFromAsset(this.getAssets(), "fonts/semibold.ttf");
-        //button.setTypeface(tf);
-        return button;
-    }
+		// Adds line and marker to the overlay
+		mv.getOverlays().add(po);
+	}
 
-    public LatLng getMapCenter()
-    {
-        return mv.getCenter();
-    }
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
 
-    public void setMapCenter(ILatLng center)
-    {
-        mv.setCenter(center);
-    }
+		return true;
+	}
 
-    /**
-     * Method to show settings  in alert dialog
-     * On pressing Settings button will lauch Settings Options - GPS
-     */
-    public void showSettingsAlert() {
-        AlertDialog.Builder alertDialog = new AlertDialog.Builder(getBaseContext());
+	private Button changeButtonTypeface(Button button) {
+		// Typeface tf = Typeface.createFromAsset(this.getAssets(),
+		// "fonts/semibold.ttf");
+		// button.setTypeface(tf);
+		return button;
+	}
 
-        // Setting Dialog Title
-        alertDialog.setTitle("GPS settings");
+	public LatLng getMapCenter() {
+		return mv.getCenter();
+	}
 
-        // Setting Dialog Message
-        alertDialog.setMessage("GPS is not enabled. Do you want to go to settings menu?");
+	public void setMapCenter(ILatLng center) {
+		mv.setCenter(center);
+	}
 
-        // On pressing Settings button
-        alertDialog.setPositiveButton("Settings", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
-                getBaseContext().startActivity(intent);
-            }
-        });
+	/**
+	 * Method to show settings in alert dialog On pressing Settings button will
+	 * lauch Settings Options - GPS
+	 */
+	public void showSettingsAlert() {
+		AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+				getBaseContext());
 
-        // on pressing cancel button
-        alertDialog.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.cancel();
-            }
-        });
+		// Setting Dialog Title
+		alertDialog.setTitle("GPS settings");
 
-        // Showing Alert Message
-        alertDialog.show();
-    }
+		// Setting Dialog Message
+		alertDialog
+				.setMessage("GPS is not enabled. Do you want to go to settings menu?");
 
+		// On pressing Settings button
+		alertDialog.setPositiveButton("Settings",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						Intent intent = new Intent(
+								Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+						getBaseContext().startActivity(intent);
+					}
+				});
+
+		// on pressing cancel button
+		alertDialog.setNegativeButton("Cancel",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int which) {
+						dialog.cancel();
+					}
+				});
+
+		// Showing Alert Message
+		alertDialog.show();
+	}
 
 }
