4f1aee789270cc936b382fb98b0516c814ad4b48
Brad Leege <bleege@gmail.com>
2014-04-01 21:25:57 -0500

    #218 - Removing @TargetApi annotations

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTileCache.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTileCache.java
@@ -1,7 +1,6 @@
 // Created by plusminus on 17:58:57 - 25.09.2008
 package com.mapbox.mapboxsdk.tileprovider;
 
-import android.annotation.TargetApi;
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -10,12 +9,9 @@ import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
-
 import com.mapbox.mapboxsdk.tileprovider.constants.TileLayerConstants;
-
 import java.io.File;
 import java.io.InputStream;
-
 import uk.co.senab.bitmapcache.BitmapLruCache;
 import uk.co.senab.bitmapcache.CacheableBitmapDrawable;
 
@@ -192,7 +188,6 @@ public class MapTileCache implements TileLayerConstants {
      * Creates a unique subdirectory of the designated app cache directory. Tries to use external
      * but if not mounted, falls back on internal storage.
      */
-    @TargetApi(Build.VERSION_CODES.GINGERBREAD)
     public static File getDiskCacheDir(Context context, String uniqueName) {
         // Check if media is mounted or storage is built-in, if so, try and use external cache dir
         // otherwise use internal cache dir
