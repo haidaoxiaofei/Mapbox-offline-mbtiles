135a2202cc64b287b656cd19e2f29b4923e16bc5
Martin Guillon <martin.guillon@akylas.fr>
2014-04-03 16:32:04 +0200

    update to go with last commit

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/MapboxTileLayer.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/MapboxTileLayer.java
@@ -1,7 +1,6 @@
 package com.mapbox.mapboxsdk.tileprovider.tilesource;
 
 import com.mapbox.mapboxsdk.constants.MapboxConstants;
-import com.mapbox.mapboxsdk.tileprovider.MapTile;
 import com.mapbox.mapboxsdk.views.util.constants.MapViewConstants;
 
 /**
@@ -17,19 +16,22 @@ public class MapboxTileLayer extends WebSourceTileLayer implements MapViewConsta
     /**
      * Initialize a new tile layer, directed at a hosted Mapbox tilesource.
      *
-     * @param id a valid mapid, of the form account.map
+     * @param pId a valid mapid, of the form account.map
      */
-    public MapboxTileLayer(String id) {
-        this(id, false);
+    public MapboxTileLayer(String pId) {
+        this(pId, false);
     }
 
-    public MapboxTileLayer(String id, boolean enableSSL) {
-        super(id, id, enableSSL);
+    public MapboxTileLayer(String pId, boolean enableSSL) {
+        super(pId, pId, enableSSL);
     }
-
+    @Override
+    protected void initialize(String pId, String aUrl, boolean enableSSL) {
+        mId = pId;
+        super.initialize(pId, aUrl, enableSSL);
+    }
     @Override
     public TileLayer setURL(final String aUrl) {
-        mId = aUrl;
         if (!aUrl.contains("http://") && !aUrl.contains("https://")
                 && aUrl.contains("")) {
             super.setURL(MAPBOX_BASE_URL + aUrl + "/{z}/{x}/{y}{2x}.png");
