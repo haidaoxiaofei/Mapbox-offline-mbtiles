8a3d372f69a9a798b1f85332230c50fa357422f0
Martin Guillon <martin.guillon@akylas.fr>
2014-04-05 10:32:59 +0200

    a test at another computation of the max memory cache size

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTileCache.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/MapTileCache.java
@@ -1,11 +1,15 @@
 // Created by plusminus on 17:58:57 - 25.09.2008
 package com.mapbox.mapboxsdk.tileprovider;
 
+import android.annotation.TargetApi;
+import android.app.ActivityManager;
 import android.content.Context;
+import android.content.pm.ApplicationInfo;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 import android.os.Environment;
 import android.util.Log;
 
@@ -39,6 +43,24 @@ public class MapTileCache implements TileLayerConstants {
         this.mMaximumCacheSize = aMaximumCacheSize;
     }
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
+    private static class ActivityManagerHoneycomb {
+        static int getLargeMemoryClass(ActivityManager activityManager) {
+            return activityManager.getLargeMemoryClass();
+        }
+    }
+
+    static int calculateMemoryCacheSize(Context context) {
+        ActivityManager am = (ActivityManager)context.getSystemService(Context.ACTIVITY_SERVICE);
+        boolean largeHeap = (context.getApplicationInfo().flags & ApplicationInfo.FLAG_LARGE_HEAP) != 0;
+        int memoryClass = am.getMemoryClass();
+        if (largeHeap && Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+            memoryClass = ActivityManagerHoneycomb.getLargeMemoryClass(am);
+        }
+        // Target ~15% of the available heap.
+        return 1024 * 1024 * memoryClass / 7;
+    }
+
     /**
      * Get the BitmapLruCache that belongs to this tile cache, creating it first
      * if there isn't one yet.
@@ -57,7 +79,7 @@ public class MapTileCache implements TileLayerConstants {
             }
             BitmapLruCache.Builder builder = new BitmapLruCache.Builder(context);
             builder.setMemoryCacheEnabled(true)
-                    .setMemoryCacheMaxSizeUsingHeapSize()
+                    .setMemoryCacheMaxSize(calculateMemoryCacheSize(context))
                     .setDiskCacheEnabled(false)
                             // 100MB
                     .setDiskCacheMaxSize(this.mMaximumCacheSize)
