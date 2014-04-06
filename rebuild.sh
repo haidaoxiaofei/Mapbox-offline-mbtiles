8aa4da8548883587976e07238ce140b6e7d69b44
Brad Leege <bleege@gmail.com>
2014-04-06 12:15:54 -0500

    Making rebuild.sh use Gradle Wrapper instead of locally installed version

--- rebuild.sh
+++ rebuild.sh
@@ -1,8 +1,9 @@
+#!/bin/sh
 # this script just joins together the commands
 # you need to build the SDK from the cli, install it on device,
 # and run it.
 cd MapboxAndroidSDK
-gradle assembleDebug install
+../gradlew assembleDebug install
 cd ../MapboxAndroidSDKTestApp
-gradle assembleDebug installDebug
+../gradlew assembleDebug installDebug
 adb shell am start -n "com.mapbox.mapboxsdk.android.testapp/com.mapbox.mapboxsdk.android.testapp.MainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
