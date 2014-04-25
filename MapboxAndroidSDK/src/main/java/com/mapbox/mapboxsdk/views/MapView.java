35386cb6139628d6f7c1625f166dba3e535e4d65
Martin Guillon <martin.guillon@akylas.fr>
2014-04-25 18:25:55 +0200

    regionFits argument to zoomToBoundingBox

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/MapView.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/MapView.java
@@ -670,7 +670,7 @@ public class MapView extends ViewGroup
      * @param boundingBox the box to compute the zoom for
      * @return the minimum zoom necessary to show the bounding box
      */
-    private float minimumZoomForBoundingBox(final BoundingBox boundingBox) {
+    private float minimumZoomForBoundingBox(final BoundingBox boundingBox, final boolean regionFits) {
         final RectF rect = Projection.toMapPixels(boundingBox, TileLayerConstants.MAXIMUM_ZOOMLEVEL,
                 mTempRect);
         final float requiredLatitudeZoom =
@@ -679,7 +679,7 @@ public class MapView extends ViewGroup
         final float requiredLongitudeZoom =
                 TileLayerConstants.MAXIMUM_ZOOMLEVEL - (float) ((Math.log(
                         rect.width() / getMeasuredWidth()) / Math.log(2)));
-        return Math.max(requiredLatitudeZoom, requiredLongitudeZoom);
+        return regionFits?Math.min(requiredLatitudeZoom, requiredLongitudeZoom):Math.max(requiredLatitudeZoom, requiredLongitudeZoom);
     }
 
     /**
@@ -688,7 +688,7 @@ public class MapView extends ViewGroup
      * will always zoom to center of zoom  level 0.
      * Suggestion: Check getScreenRect(null).getHeight() > 0
      */
-    public MapView zoomToBoundingBox(final BoundingBox boundingBox) {
+    public MapView zoomToBoundingBox(final BoundingBox boundingBox, final boolean regionFits) {
         BoundingBox inter =
                 (mScrollableAreaBoundingBox != null) ? mScrollableAreaBoundingBox.intersect(
                         boundingBox) : boundingBox;
@@ -701,12 +701,15 @@ public class MapView extends ViewGroup
         }
 
         // Zoom to boundingBox center, at calculated maximum allowed zoom level
-        getController().setZoom(minimumZoomForBoundingBox(inter));
+        getController().setZoom(minimumZoomForBoundingBox(inter, regionFits));
 
         getController().setCenter(
                 new LatLng(inter.getCenter().getLatitude(), inter.getCenter().getLongitude()));
 
         return this;
+    }
+    public MapView zoomToBoundingBox(final BoundingBox boundingBox) {
+    	return zoomToBoundingBox(boundingBox, false);
     }
 
     public float getClampedZoomLevel(float zoom) {
@@ -866,7 +869,7 @@ public class MapView extends ViewGroup
             return;
         }
         mMinimumZoomLevel = (float) Math.max(mRequestedMinimumZoomLevel,
-                minimumZoomForBoundingBox(mScrollableAreaBoundingBox));
+                minimumZoomForBoundingBox(mScrollableAreaBoundingBox, false));
         if (mZoomLevel < mMinimumZoomLevel) {
             setZoom(mMinimumZoomLevel);
         }
