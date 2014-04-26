f70e43b663e03da2958c9891ee3409b249ecb2e9
Martin Guillon <martin.guillon@akylas.fr>
2014-04-26 19:51:19 +0200

    bug fix the test for nullOrEmpty should be !

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/MapboxTileLayer.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/tileprovider/tilesource/MapboxTileLayer.java
@@ -3,6 +3,7 @@ package com.mapbox.mapboxsdk.tileprovider.tilesource;
 import com.google.common.base.Strings;
 import com.mapbox.mapboxsdk.constants.MapboxConstants;
 import com.mapbox.mapboxsdk.views.util.constants.MapViewConstants;
+
 import java.util.Locale;
 
 /**
@@ -36,8 +37,9 @@ public class MapboxTileLayer extends TileJsonTileLayer
 
     @Override
     public TileLayer setURL(final String aUrl) {
-        if (!aUrl.toLowerCase(Locale.US).contains("http://") && !aUrl.toLowerCase(Locale.US)
-                .contains("https://") && Strings.isNullOrEmpty(aUrl)) {
+        if (!Strings.isNullOrEmpty(aUrl) &&
+                !aUrl.toLowerCase(Locale.US).contains("http://") && !aUrl.toLowerCase(Locale.US)
+                .contains("https://")) {
             super.setURL(MAPBOX_BASE_URL + aUrl + "/{z}/{x}/{y}{2x}.png");
         } else {
             super.setURL(aUrl);
