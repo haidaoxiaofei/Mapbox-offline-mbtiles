d39bb983761f96d89625981b5817f7d3b8e38913
Brad Leege <bleege@gmail.com>
2014-05-04 19:09:52 -0500

    #349 - Adding dynamic generation and loading of markers on map

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MarkersTestActivity.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MarkersTestActivity.java
@@ -7,16 +7,35 @@ package com.mapbox.mapboxsdk.android.testapp;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.overlay.Icon;
+import com.mapbox.mapboxsdk.overlay.Marker;
 import com.mapbox.mapboxsdk.views.MapView;
+import java.util.Random;
 
-public class MarkersTestActivity extends ActionBarActivity
-{
+public class MarkersTestActivity extends ActionBarActivity {
+
+	private MapView mapView;
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_markers);
-		MapView mv = (MapView) findViewById(R.id.markersMapView);
-		mv.setCenter(new LatLng(38.11493876707079, 13.3647260069847));
-		mv.setZoom(14);
+		mapView = (MapView) findViewById(R.id.markersMapView);
+		mapView.setCenter(new LatLng(38.11493876707079, 13.3647260069847));
+		mapView.setZoom(14);
+
+		// Dynamically create 100 markers
+		Random r = new Random();
+		for(int i = 0; i < 100; i++) {
+			LatLng position = new LatLng(new LatLng(38.11493876707079f + r.nextFloat() / 100,
+					13.3647260069847f  + r.nextFloat() / 100));
+			addMarker(position);
+		}
+	}
+
+	public void addMarker(LatLng position) {
+		Marker marker = new Marker(mapView, "", "", position);
+		marker.setIcon(new Icon(this, Icon.Size.SMALL, "marker-stroked", "FF0000"));
+		mapView.addMarker(marker);
 	}
 }
