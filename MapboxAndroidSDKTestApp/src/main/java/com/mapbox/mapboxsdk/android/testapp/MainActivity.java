04762cee13e0d59e38a8058b992e21a64d427991
Brad Leege <bleege@gmail.com>
2014-05-13 18:18:52 -0500

    #350 - Setting up DrawerListener and initial loading of MainTestFragment

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MainActivity.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MainActivity.java
@@ -1,14 +1,21 @@
 package com.mapbox.mapboxsdk.android.testapp;
 
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import java.util.ArrayList;
 
 public class MainActivity extends ActionBarActivity {
 
 	private DrawerLayout mDrawerLayout;
 	private ListView mDrawerList;
+	private ArrayList<String> testFragmentNames;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,5 +24,56 @@ public class MainActivity extends ActionBarActivity {
 
 		mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
 		mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+		// Set the adapter for the list view
+		testFragmentNames = new ArrayList<String>();
+		testFragmentNames.add(getString(R.string.app_name));
+		testFragmentNames.add(getString(R.string.alternateTestMapActivity));
+		testFragmentNames.add(getString(R.string.markersTestActivity));
+
+		mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, testFragmentNames));
+		// Set the list's click listener
+		mDrawerList.setOnItemClickListener(new DrawerItemClickListener());
+
+		// Set MainTestFragment
+		selectItem(0);
     }
+
+	private class DrawerItemClickListener implements ListView.OnItemClickListener {
+		@Override
+		public void onItemClick(AdapterView parent, View view, int position, long id) {
+			selectItem(position);
+		}
+	}
+
+	/** Swaps fragments in the main content view */
+	private void selectItem(int position) {
+		// Create a new fragment and specify the planet to show based on position
+		Fragment fragment;
+
+		switch (position){
+			case 0:
+				fragment = new MainTestFragment();
+				break;
+			default:
+				fragment = new MainTestFragment();
+				break;
+		}
+
+		// Insert the fragment by replacing any existing fragment
+		FragmentManager fragmentManager = getSupportFragmentManager();
+		fragmentManager.beginTransaction()
+				.replace(R.id.content_frame, fragment)
+				.commit();
+
+		// Highlight the selected item, update the title, and close the drawer
+		mDrawerList.setItemChecked(position, true);
+		setTitle(testFragmentNames.get(position));
+		mDrawerLayout.closeDrawer(mDrawerList);
+	}
+
+	@Override
+	public void setTitle(CharSequence title) {
+		getSupportActionBar().setTitle(title);
+	}
 }
