40130da03d782563e2f80721ce05da0428ee00a2
Brad Leege <bleege@gmail.com>
2014-05-15 12:07:47 -0500

    #350 - Renaming former activity layouts to fragment layouts

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/AlternateMapTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/AlternateMapTestFragment.java
@@ -16,7 +16,7 @@ import com.mapbox.mapboxsdk.views.MapView;
 public class AlternateMapTestFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-		View view = inflater.inflate(R.layout.activity_alternatemap, container, false);
+		View view = inflater.inflate(R.layout.fragment_alternatemap, container, false);
 
 		MapView mv = (MapView) view.findViewById(R.id.altMapView);
 		mv.setCenter(new LatLng(43.07472, -89.38421));
--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MarkersTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/MarkersTestFragment.java
@@ -18,7 +18,7 @@ public class MarkersTestFragment extends Fragment {
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
 	{
-		View view = inflater.inflate(R.layout.activity_markers, container, false);
+		View view = inflater.inflate(R.layout.fragment_markers, container, false);
 
 		// Setup Map
 		mapView = (MapView) view.findViewById(R.id.markersMapView);
