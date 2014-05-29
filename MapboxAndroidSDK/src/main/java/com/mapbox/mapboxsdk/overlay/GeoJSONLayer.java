c8ab2a7b2d1d3bfd26fba218752239339335c5ac
Brad Leege <bleege@gmail.com>
2014-05-28 20:35:11 -0500

    #205 - Initial refactoring of GeoJSONLayer to use the android-geojson library

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/GeoJSONLayer.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/GeoJSONLayer.java
@@ -1,11 +1,17 @@
 package com.mapbox.mapboxsdk.overlay;
 
+import android.graphics.Paint;
 import android.os.AsyncTask;
 import android.util.Log;
+import com.cocoahero.android.geojson.Feature;
+import com.cocoahero.android.geojson.FeatureCollection;
+import com.cocoahero.android.geojson.GeoJSON;
 import com.google.common.base.Strings;
-import com.mapbox.mapboxsdk.format.GeoJSON;
+import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.util.constants.UtilConstants;
 import com.mapbox.mapboxsdk.views.MapView;
+import org.json.JSONArray;
+import org.json.JSONException;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
@@ -14,14 +20,16 @@ import java.io.Reader;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.util.ArrayList;
-import java.util.List;
 
 public class GeoJSONLayer {
 
     private final MapView mapView;
+    private final Icon markerIcon;
 
-    public GeoJSONLayer(final MapView mapView) {
+    public GeoJSONLayer(final MapView mapView, final Icon markerIcon) {
+        super();
         this.mapView = mapView;
+        this.markerIcon = markerIcon;
     }
 
     public void loadURL(final String url) {
@@ -38,8 +46,9 @@ public class GeoJSONLayer {
         @Override
         protected ArrayList<Object> doInBackground(String... params) {
             InputStream is;
-            String jsonText = null;
+            String jsonText;
             ArrayList<Object> uiObjects = new ArrayList<Object>();
+
             try {
                 if (UtilConstants.DEBUGMODE) {
                     Log.d(TAG, "Mapbox SDK downloading GeoJSON URL: " + params[0]);
@@ -49,12 +58,130 @@ public class GeoJSONLayer {
                         new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")));
                 jsonText = readAll(rd);
 
-                List<Object> parsed = GeoJSON.parseString(jsonText, mapView);
+                FeatureCollection parsed = (FeatureCollection) GeoJSON.parse(jsonText);
                 if (UtilConstants.DEBUGMODE) {
-                    Log.d(TAG, "Parsed GeoJSON with " + parsed.size() + " features.");
+                    Log.d(TAG, "Parsed GeoJSON with " + parsed.getFeatures().size() + " features.");
                 }
 
-                uiObjects.addAll(parsed);
+                for (Feature f : parsed.getFeatures()) {
+                    // Parse Into UI Objections
+                    String type = f.getType();
+                    int j;
+
+                    if (type.equals("Point")) {
+                        JSONArray coordinates = (JSONArray) f.toJSON().get("coordinates");
+                        double lon = (Double) coordinates.get(0);
+                        double lat = (Double) coordinates.get(1);
+                        Marker marker = new Marker(mapView, "", "", new LatLng(lat, lon));
+                        if (markerIcon != null) {
+                            marker.setIcon(markerIcon);
+                        }
+                        uiObjects.add(marker);
+                    } else if (type.equals("MultiPoint")) {
+                        JSONArray points = (JSONArray) f.toJSON().get("coordinates");
+                        for (j = 0; j < points.length(); j++) {
+                            JSONArray coordinates = (JSONArray) points.get(j);
+                            double lon = (Double) coordinates.get(0);
+                            double lat = (Double) coordinates.get(1);
+                            Marker marker = new Marker(mapView, "", "", new LatLng(lat, lon));
+                            if (markerIcon != null) {
+                                marker.setIcon(markerIcon);
+                            }
+                            uiObjects.add(marker);
+                        }
+                    } else if (type.equals("LineString")) {
+                        PathOverlay path = new PathOverlay();
+                        JSONArray points = (JSONArray) f.toJSON().get("coordinates");
+                        JSONArray coordinates;
+                        for (j = 0; j < points.length(); j++) {
+                            coordinates = (JSONArray) points.get(j);
+                            double lon = (Double) coordinates.get(0);
+                            double lat = (Double) coordinates.get(1);
+                            path.addPoint(new LatLng(lat, lon));
+                        }
+                        uiObjects.add(path);
+                    } else if (type.equals("MultiLineString")) {
+                        JSONArray lines = (JSONArray) f.toJSON().get("coordinates");
+                        for (int k = 0; k < lines.length(); k++) {
+                            PathOverlay path = new PathOverlay();
+                            JSONArray points = (JSONArray) lines.get(k);
+                            JSONArray coordinates;
+                            for (j = 0; j < points.length(); j++) {
+                                coordinates = (JSONArray) points.get(j);
+                                double lon = (Double) coordinates.get(0);
+                                double lat = (Double) coordinates.get(1);
+                                path.addPoint(new LatLng(lat, lon));
+                            }
+                            uiObjects.add(path);
+                        }
+                    } else if (type.equals("Polygon")) {
+                        PathOverlay path = new PathOverlay();
+                        path.getPaint().setStyle(Paint.Style.FILL);
+                        JSONArray points = (JSONArray) f.toJSON().get("coordinates");
+
+                        for (int r = 0; r < points.length(); r++) {
+                            JSONArray ring = (JSONArray) points.get(r);
+                            JSONArray coordinates;
+
+                            // we re-wind inner rings of GeoJSON polygons in order
+                            // to render them as transparent in the canvas layer.
+
+                            // first ring should have windingOrder = true,
+                            // all others should have winding order == false
+                            if ((r == 0 && !windingOrder(ring)) || (r != 0 && windingOrder(ring))) {
+                                for (j = 0; j < ring.length(); j++) {
+                                    coordinates = (JSONArray) ring.get(j);
+                                    double lon = (Double) coordinates.get(0);
+                                    double lat = (Double) coordinates.get(1);
+                                    path.addPoint(new LatLng(lat, lon));
+                                }
+                            } else {
+                                for (j = ring.length() - 1; j >= 0; j--) {
+                                    coordinates = (JSONArray) ring.get(j);
+                                    double lon = (Double) coordinates.get(0);
+                                    double lat = (Double) coordinates.get(1);
+                                    path.addPoint(new LatLng(lat, lon));
+                                }
+                            }
+                            uiObjects.add(path);
+                        }
+                    } else if (type.equals("MultiPolygon")) {
+                        PathOverlay path = new PathOverlay();
+                        path.getPaint().setStyle(Paint.Style.FILL);
+                        JSONArray polygons = (JSONArray) f.toJSON().get("coordinates");
+
+                        for (int p = 0; p < polygons.length(); p++) {
+                            JSONArray points = (JSONArray) polygons.get(p);
+                            for (int r = 0; r < points.length(); r++) {
+                                JSONArray ring = (JSONArray) points.get(r);
+                                JSONArray coordinates;
+
+                                // we re-wind inner rings of GeoJSON polygons in order
+                                // to render them as transparent in the canvas layer.
+
+                                // first ring should have windingOrder = true,
+                                // all others should have winding order == false
+                                if ((r == 0 && !windingOrder(ring)) || (r != 0 && windingOrder(ring))) {
+                                    for (j = 0; j < ring.length(); j++) {
+                                        coordinates = (JSONArray) ring.get(j);
+                                        double lon = (Double) coordinates.get(0);
+                                        double lat = (Double) coordinates.get(1);
+                                        path.addPoint(new LatLng(lat, lon));
+                                    }
+                                } else {
+                                    for (j = ring.length() - 1; j >= 0; j--) {
+                                        coordinates = (JSONArray) ring.get(j);
+                                        double lon = (Double) coordinates.get(0);
+                                        double lat = (Double) coordinates.get(1);
+                                        path.addPoint(new LatLng(lat, lon));
+                                    }
+                                }
+                                uiObjects.add(path);
+                            }
+                        }
+                    }
+                }
+
             } catch (Exception e) {
                 Log.e(TAG, "Error loading / parsing GeoJSON: " + e.toString());
                 e.printStackTrace();
@@ -76,15 +203,34 @@ public class GeoJSONLayer {
                 mapView.invalidate();
             }
         }
+    }
 
-        private String readAll(Reader rd) throws IOException {
-            StringBuilder sb = new StringBuilder();
-            int cp;
-            while ((cp = rd.read()) != -1) {
-                sb.append((char) cp);
-            }
-            return sb.toString();
+    private String readAll(Reader rd) throws IOException {
+        StringBuilder sb = new StringBuilder();
+        int cp;
+        while ((cp = rd.read()) != -1) {
+            sb.append((char) cp);
         }
+        return sb.toString();
+    }
+
+    private static boolean windingOrder(JSONArray ring) throws JSONException {
+        float area = 0;
+
+        if (ring.length() > 2) {
+            for (int i = 0; i < ring.length() - 1; i++) {
+                JSONArray p1 = (JSONArray) ring.get(i);
+                JSONArray p2 = (JSONArray) ring.get(i + 1);
+                area += rad((Double) p2.get(0) - (Double) p1.get(0)) * (2 + Math.sin(
+                        rad((Double) p1.get(1))) + Math.sin(rad((Double) p2.get(1))));
+            }
+        }
+
+        return area > 0;
+    }
+
+    private static double rad(double _) {
+        return _ * Math.PI / 180f;
     }
 
     static final String TAG = "GeoJSONLayer";
