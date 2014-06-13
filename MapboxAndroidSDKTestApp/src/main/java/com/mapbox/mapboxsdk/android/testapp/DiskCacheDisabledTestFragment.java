1d4c2f32ea39cdb0315e8d78b1b8f5697605a8f5
Brad Leege <bleege@gmail.com>
2014-06-12 22:11:15 -0500

    #401 - Fixing checkstyle issue

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/DiskCacheDisabledTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/DiskCacheDisabledTestFragment.java
@@ -8,8 +8,7 @@ import android.view.ViewGroup;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
-public class DiskCacheDisabledTestFragment extends Fragment
-{
+public class DiskCacheDisabledTestFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_diskcachedisabled, container, false);
