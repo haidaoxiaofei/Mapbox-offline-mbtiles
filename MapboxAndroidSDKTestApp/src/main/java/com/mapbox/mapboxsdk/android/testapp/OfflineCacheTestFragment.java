d529217010fabb2a515f1f9809a5c1568a5a641c
Brad Leege <bleege@gmail.com>
2014-06-15 19:49:05 -0500

    #229 - Adding Data On / Data Off button to Offline Cache Test Map that clears Memory Cache so that tiles can only come from Disk Cache

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/OfflineCacheTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/OfflineCacheTestFragment.java
@@ -5,6 +5,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
@@ -14,10 +15,29 @@ public class OfflineCacheTestFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_offlinecache, container, false);
 
         // Setup Map
-        MapView mapView = (MapView) view.findViewById(R.id.offlineCacheMapView);
+        final MapView mapView = (MapView) view.findViewById(R.id.offlineCacheMapView);
         mapView.setDiskCacheEnabled(true);
         mapView.setCenter(new LatLng(46.85268, -121.75907));
         mapView.setZoom(10);
+
+        final Button dataButton = (Button) view.findViewById(R.id.dataOnOffButton);
+        if (!mapView.useDataConnection()) {
+            dataButton.setText(R.string.dataOff);
+        }
+        dataButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (dataButton.getText().equals(getString(R.string.dataOn))) {
+                    mapView.setUseDataConnection(false);
+                    mapView.getTileProvider().clearTileMemoryCache();
+                    dataButton.setText(R.string.dataOff);
+                } else {
+                    mapView.setUseDataConnection(true);
+                    dataButton.setText(R.string.dataOn);
+                }
+            }
+        });
+
 
         return view;
     }
