735285c9be77d389fe8fd0a0d7c101015d5f5850
Damian Wieczorek <damianw@umich.edu>
2014-08-28 17:44:22 -0400

    changed to PointF(0, 0) instead of null check

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/MapController.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/views/MapController.java
@@ -115,9 +115,7 @@ public class MapController implements MapViewConstants {
 
         final Projection projection = mMapView.getProjection();
         PointF p = projection.toMapPixels(point, null);
-        if (delta != null) {
-            p.offset(delta.x, delta.y);
-        }
+        p.offset(delta.x, delta.y);
         if (mMapView.getScrollPoint().equals(p)) {
             return false;
         }
