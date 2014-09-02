eff8038ef270b6be633bd527ddf96a0758a95705
o_obushenko <oobushen@hawk.iit.edu>
2014-09-02 15:19:46 -0500

    Changed color methods used to superior getColor/setColor methods that only require one integer.

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/UserLocationOverlay.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/overlay/UserLocationOverlay.java
@@ -85,15 +85,15 @@ public class UserLocationOverlay extends SafeDrawOverlay implements Snappable, M
         mPersonHotspot = point;
     }
 
-    public void setOverlayCircleColor(int alpha, int red, int green, int blue){
-        mCirclePaint.setARGB(alpha, red, green, blue); 
+    public void setOverlayCircleColor(int newColor){
+        mCirclePaint.setColor(newColor);
     }
 
     public UserLocationOverlay(GpsLocationProvider myLocationProvider, MapView mapView, int arrowId, int personId) {
         mMapView = mapView;
         mMapController = mapView.getController();
         mContext = mapView.getContext();
-        mCirclePaint.setARGB(0, 100, 100, 255);
+        mCirclePaint.setColor(0x776464FF);
         mCirclePaint.setAntiAlias(true);
         mPaint.setAntiAlias(true);
         mPaint.setFilterBitmap(true);
