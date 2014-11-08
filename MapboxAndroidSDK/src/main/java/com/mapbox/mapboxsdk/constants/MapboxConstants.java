ec773c729e9c7ecc15d39b49d91d8515ec5e594d
Brad Leege <bleege@gmail.com>
2014-11-08 17:33:57 -0600

    #533 - Implementing OfflineMapDatabase.initializeDatabase().  Adding supporting data types.

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
@@ -15,20 +15,53 @@ public interface MapboxConstants {
 
     public enum RasterImageQuality {
         /** Full image quality. */
-        MBXRasterImageQualityFull,
+        MBXRasterImageQualityFull(0),
         /** 32 color indexed PNG. */
-        MBXRasterImageQualityPNG32,
+        MBXRasterImageQualityPNG32(1),
         /** 64 color indexed PNG. */
-        MBXRasterImageQualityPNG64,
+        MBXRasterImageQualityPNG64(2),
         /** 128 color indexed PNG. */
-        MBXRasterImageQualityPNG128,
+        MBXRasterImageQualityPNG128(3),
         /** 256 color indexed PNG. */
-        MBXRasterImageQualityPNG256,
+        MBXRasterImageQualityPNG256(4),
         /** 70% quality JPEG. */
-        MBXRasterImageQualityJPEG70,
+        MBXRasterImageQualityJPEG70(5),
         /** 80% quality JPEG. */
-        MBXRasterImageQualityJPEG80,
+        MBXRasterImageQualityJPEG80(6),
         /** 90% quality JPEG. */
-        MBXRasterImageQualityJPEG90
+        MBXRasterImageQualityJPEG90(7);
+
+        private int value;
+
+        private RasterImageQuality(int value) {
+            this.value = value;
+        }
+
+        public int getValue() {
+            return value;
+        }
+
+        public static RasterImageQuality getEnumForValue(int value) {
+            switch (value) {
+                case 0:
+                    return MBXRasterImageQualityFull;
+                case 1:
+                    return MBXRasterImageQualityPNG32;
+                case 2:
+                    return MBXRasterImageQualityPNG64;
+                case 3:
+                    return MBXRasterImageQualityPNG128;
+                case 4:
+                    return MBXRasterImageQualityPNG256;
+                case 5:
+                    return MBXRasterImageQualityJPEG70;
+                case 6:
+                    return MBXRasterImageQualityJPEG80;
+                case 7:
+                    return MBXRasterImageQualityJPEG90;
+                default:
+                    return MBXRasterImageQualityFull;
+            }
+        }
     }
 }
--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineMapDatabase.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/offline/OfflineMapDatabase.java
@@ -3,8 +3,13 @@ package com.mapbox.mapboxsdk.offline;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.text.TextUtils;
+import android.util.Log;
 import com.mapbox.mapboxsdk.constants.MapboxConstants;
-import com.mapbox.mapboxsdk.geometry.BoundingBox;
+import com.mapbox.mapboxsdk.geometry.CoordinateRegion;
+import com.mapbox.mapboxsdk.geometry.CoordinateSpan;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import java.util.Date;
 
 public class OfflineMapDatabase implements MapboxConstants {
 
@@ -17,12 +22,12 @@ public class OfflineMapDatabase implements MapboxConstants {
     private boolean includesMetadata;
     private boolean includesMarkers;
     private RasterImageQuality imageQuality;
-    private BoundingBox mapRegion;
+    private CoordinateRegion mapRegion;
     private Integer minimumZ;
     private Integer maximumZ;
     private String path;
     private boolean invalid;
-    private boolean initializedProperly;
+    private boolean initializedProperly = false;
 
     /**
      * Default Constructor
@@ -33,6 +38,60 @@ public class OfflineMapDatabase implements MapboxConstants {
         this.context = context;
     }
 
+    public boolean initializeDatabase() {
+
+        String uniqueID = sqliteMetadataForName("uniqueID");
+        String mapID = sqliteMetadataForName("mapID");
+        String includesMetadata = sqliteMetadataForName("includesMetadata");
+        String includesMarkers = sqliteMetadataForName("includesMarkers");
+        String imageQuality = sqliteMetadataForName("imageQuality");
+        String region_latitude = sqliteMetadataForName("region_latitude");
+        String region_longitude = sqliteMetadataForName("region_longitude");
+        String region_latitude_delta = sqliteMetadataForName("region_latitude_delta");
+        String region_longitude_delta = sqliteMetadataForName("region_longitude_delta");
+        String minimumZ = sqliteMetadataForName("minimumZ");
+        String maximumZ = sqliteMetadataForName("maximumZ");
+
+        if (TextUtils.isEmpty(uniqueID)) {
+            uniqueID = String.format("%s-%s-%s-%s-%s-%s-%s-%d", mapID, region_latitude, region_longitude, region_latitude_delta, region_longitude_delta, minimumZ, maximumZ, new Date().getTime() / 1000l);
+        }
+
+        if (!TextUtils.isEmpty(mapID) && !TextUtils.isEmpty(includesMetadata) && !TextUtils.isEmpty(includesMarkers) && !TextUtils.isEmpty(imageQuality)
+                && !TextUtils.isEmpty(region_latitude) && !TextUtils.isEmpty(region_longitude) && !TextUtils.isEmpty(region_latitude_delta) && !TextUtils.isEmpty(region_longitude_delta)
+                && !TextUtils.isEmpty(minimumZ) && !TextUtils.isEmpty(maximumZ)
+                ) {
+            // Reaching this point means that the specified database file at path pointed to an sqlite file which had
+            // all the required values in its metadata table. That means the file passed the test for being a valid
+            // offline map database.
+            //
+            this.uniqueID = uniqueID;
+            this.mapID = mapID;
+            this.includesMetadata = Boolean.getBoolean(includesMetadata);
+            this.includesMarkers = Boolean.getBoolean(includesMarkers);
+
+            this.imageQuality = RasterImageQuality.getEnumForValue(Integer.getInteger(imageQuality));
+
+            LatLng center = new LatLng(Double.parseDouble(region_latitude), Double.parseDouble(region_longitude));
+            CoordinateSpan span = new CoordinateSpan(Double.parseDouble(region_latitude_delta), Double.parseDouble(region_longitude_delta));
+            this.mapRegion = new CoordinateRegion(center, span);
+
+            this.minimumZ = Integer.getInteger(minimumZ);
+            this.maximumZ = Integer.getInteger(maximumZ);
+
+            this.initializedProperly = true;
+        }
+        else
+        {
+            // Reaching this point means the file at path isn't a valid offline map database, so we can't use it.
+            Log.w(TAG, "Invalid offline map database.  Can't be used.");
+        }
+        return initializedProperly;
+    }
+
+    public void invalidate() {
+        this.invalid = false;
+    }
+
     public String sqliteMetadataForName (String name)
     {
         String query = "SELECT " + OfflineDatabaseHandler.FIELD_METADATA_VALUE + " FROM " + OfflineDatabaseHandler.TABLE_METADATA +  " WHERE " + OfflineDatabaseHandler.FIELD_METADATA_NAME + "='" + name + "';";
@@ -40,7 +99,6 @@ public class OfflineMapDatabase implements MapboxConstants {
         Cursor cursor = db.rawQuery(query, null);
         return cursor.getString(cursor.getColumnIndex(OfflineDatabaseHandler.FIELD_METADATA_VALUE));
     }
-
 
     public byte[] sqliteDataForURL(String url)
     {
