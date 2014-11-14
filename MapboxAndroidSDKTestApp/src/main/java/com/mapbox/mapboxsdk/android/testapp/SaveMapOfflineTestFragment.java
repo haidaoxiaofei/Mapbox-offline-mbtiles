20707a459a0d7ccfd580e6e6b026c0758ea386d7
Brad Leege <bleege@gmail.com>
2014-11-14 17:19:56 -0600

    #533 - Adding Save Map button to UI

--- MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/SaveMapOfflineTestFragment.java
+++ MapboxAndroidSDKTestApp/src/main/java/com/mapbox/mapboxsdk/android/testapp/SaveMapOfflineTestFragment.java
@@ -2,13 +2,17 @@ package com.mapbox.mapboxsdk.android.testapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.views.MapView;
 
 public class SaveMapOfflineTestFragment extends Fragment {
+
+    private static final String TAG = "SaveMapOfflineTestFragment";
 
     private MapView mapView;
 
@@ -21,6 +25,18 @@ public class SaveMapOfflineTestFragment extends Fragment {
         mapView.setCenter(new LatLng(29.94423, -90.09201));
         mapView.setZoom(12);
 
+        Button saveMapButton = (Button) view.findViewById(R.id.saveMapButton);
+        saveMapButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleSaveMapButton(v);
+            }
+        });
+
         return view;
+    }
+
+    public void handleSaveMapButton(View view) {
+        Log.i(TAG, "handleSaveMapButton() called");
     }
 }
