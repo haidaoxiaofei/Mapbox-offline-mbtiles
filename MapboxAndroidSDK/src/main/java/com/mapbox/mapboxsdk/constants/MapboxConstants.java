3978fac6ef5fd8afd0f2de391806ab796af8cab0
Brad Leege <bleege@gmail.com>
2014-11-17 19:23:07 -0600

    #541 - Updating User Agent for 0.5.0 Release

--- MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
+++ MapboxAndroidSDK/src/main/java/com/mapbox/mapboxsdk/constants/MapboxConstants.java
@@ -11,5 +11,5 @@ public interface MapboxConstants {
      */
     public static final String MAPBOX_BASE_URL = "https://a.tiles.mapbox.com/v3/";
 
-    public static final String USER_AGENT = "Mapbox Android SDK/0.4.0";
+    public static final String USER_AGENT = "Mapbox Android SDK/0.5.0";
 }
